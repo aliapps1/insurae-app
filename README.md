77b1c9196a7a12457fdb3e14b62f68fee6bf392d
aliapps1 <Abdulalish1970@gmail.com>
2026-05-06 10:55:52 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Insurae Android Ready
+
+Upload all files to GitHub. Then run Actions > Build Android APK.
---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'com.aliapps1.insurae',
+  appName: 'Insurae',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+export default config;
